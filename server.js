93e9e86efbca76ccde11c5fabc913b6f3e9ce67c
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-03-16 20:16:34 +0200

    initial commit; basic server code was added; added connection to MongoDB(mLab)

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+require('dotenv').config();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
