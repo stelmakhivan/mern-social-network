8f8a7b06ef476eb21c491823ab1bc955136e7403
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-03-23 20:41:49 +0200

    Added delete request for user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -295,4 +295,21 @@ router.delete(
   }
 );
 
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+      .then(() => {
+        User.findByIdAndRemove({ _id: req.user.id })
+          .then(() => res.json({ success: true }))
+          .catch(err => res.status(400).json(err));
+      })
+      .catch(err => res.status(400).json(err));
+  }
+);
+
 module.exports = router;
