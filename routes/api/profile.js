999f52b179cba2b47c62c2cc8caf4e2aa344d8c5
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-04-06 21:50:20 +0300

    added react-router-dom and fixed profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -62,7 +62,7 @@ router.get('/all', (req, res) => {
 // @desc    Get profile by handle
 // @access  Public
 
-router.get('/handel/:handle', (req, res) => {
+router.get('/handle/:handle', (req, res) => {
   const errors = {};
 
   Profile.findOne({ handle: req.params.handle })
