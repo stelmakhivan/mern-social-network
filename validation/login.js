a72f901045bcf6710a8bbd5f6df1dd56e9aac06b
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-04-14 21:40:30 +0300

    Fixed validation for login route

--- validation/login.js
+++ validation/login.js
@@ -19,10 +19,6 @@ module.exports = function validateLoginInput(data) {
     errors.password = 'Password field is required';
   }
 
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must be at least 6 characters';
-  }
-
   return {
     errors,
     isValid: isEmpty(errors)
